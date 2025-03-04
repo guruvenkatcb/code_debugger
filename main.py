00e2b0a6ab574da1feb34e50432da6358491e99a
guruvenkatcb <guruvenkat12007@gmail.com>
2025-03-04 21:55:03 +0530

    commited

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Welcome to AI Debugger!"}
+
+@app.get("/predict")
+def predict():
+    return {"prediction": "This is a sample prediction"}
 
